a9291d7372767eff66647531bb63836fd2a9120b
abdelghaniDEV <abdelghaniberhouch2001@gmail.com>
2024-12-13 11:47:18 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ import "react-multi-carousel/lib/styles.css";
 import ListCategories from "./components/ListCategories";
 import meals003 from "./assets/meals0003.png";
 import burger from "./assets/burger.jpg";
-import meals004 from "./assets/mels0005.avif"
-import meals0005 from "./assets/meals0004.avif"
+import meals004 from "./assets/mels0005.avif";
+import meals0005 from "./assets/meals0004.avif";
 
 function App() {
   const responsive = {
@@ -37,45 +37,64 @@ function App() {
   };
 
   return (
-    <div className="container">
+    <div className="">
       <Header />
       <div>
-        <h1 className="font-[400] text-[33px] leading-[38px]">
-          What are you going to eat today ?
-        </h1>
-        <div className="relative my-3">
-          <input
-            type="text"
-            placeholder="Search Here ..."
-            className="h-10 rounded-[20px] text-base w-full bg-[#F5F5F8] px-4 py-2 focus:outline-none "
-          />
-          <Search className="absolute right-[10px] text-[#BB86CF] top-[10px]" />
-        </div>
-        <div className="my-5">
-          <div className="flex gap-3 text-[13px] font-[400]">
-            {/* <Button className=" py-2 px-3 rounded-[20px] bg-[#F5F5F8]">
-              All
-            </Button>
-            <Button className=" py-2 px-3 rounded-[20px] bg-[#F5F5F8] ">
-              Burger
-            </Button>
-            <Button className="py-2 px-3 rounded-[20px] bg-[#F5F5F8]">
-              HotDog
-            </Button>
-            <Button className=" py-2 px-3 rounded-[20px]  bg-[#F5F5F8]">
-              Salade
-            </Button> */}
-            
+        <div className="container">
+          <h1 className="font-[400] text-[33px] leading-[38px]">
+            What are you going to eat today ?
+          </h1>
+          <div className="relative my-3">
+            <input
+              type="text"
+              placeholder="Search Here ..."
+              className="h-10 rounded-[20px] text-base w-full bg-[#F5F5F8] px-4 py-2 focus:outline-none "
+            />
+            <Search className="absolute right-[10px] text-[#BB86CF] top-[10px]" />
           </div>
-          <ListCategories />
         </div>
         <div>
-          <div className="grid grid-cols-1 gap-5 ">
-            <ProductCart image={meals0005} name={"Salade Nicoise"} description={"Une sélection de magasins et de chaînes de marque connus de tous afin de vous servir au mieux. Découvrez toutes les catégories"} price={37.55} />
-            <ProductCart image={burger} name={"Burger Checken"} description={"Une sélection de magasins et de chaînes de marque connus de tous afin de vous servir au mieux. Découvrez toutes les catégories"} price={40.55} />
-            <ProductCart image={meals004} name={"Pitza Mozarila"} description={"Une sélection de magasins et de chaînes de marque connus de tous afin de vous servir au mieux. Découvrez toutes les catégories"} price={80.55} />
-            <ProductCart image={meals0005} name={"Salade Nicoise"} description={"Une sélection de magasins et de chaînes de marque connus de tous afin de vous servir au mieux. Découvrez toutes les catégories"} price={37.55} />
-            <ProductCart image={burger} name={"Burger Checken"} description={"Une sélection de magasins et de chaînes de marque connus de tous afin de vous servir au mieux. Découvrez toutes les catégories"} price={40.55} />
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5 ">
+            <ProductCart
+              image={meals0005}
+              name={"Salade Nicoise"}
+              description={
+                "Une sélection de magasins et de chaînes de marque connus de tous afin de vous servir au mieux. Découvrez toutes les catégories"
+              }
+              price={37.55}
+            />
+            <ProductCart
+              image={burger}
+              name={"Burger Checken"}
+              description={
+                "Une sélection de magasins et de chaînes de marque connus de tous afin de vous servir au mieux. Découvrez toutes les catégories"
+              }
+              price={40.55}
+            />
+            <ProductCart
+              image={meals004}
+              name={"Pitza Mozarila"}
+              description={
+                "Une sélection de magasins et de chaînes de marque connus de tous afin de vous servir au mieux. Découvrez toutes les catégories"
+              }
+              price={80.55}
+            />
+            <ProductCart
+              image={meals0005}
+              name={"Salade Nicoise"}
+              description={
+                "Une sélection de magasins et de chaînes de marque connus de tous afin de vous servir au mieux. Découvrez toutes les catégories"
+              }
+              price={37.55}
+            />
+            <ProductCart
+              image={burger}
+              name={"Burger Checken"}
+              description={
+                "Une sélection de magasins et de chaînes de marque connus de tous afin de vous servir au mieux. Découvrez toutes les catégories"
+              }
+              price={40.55}
+            />
           </div>
         </div>
       </div>
--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "./ui/card";
 import meals003 from "../assets/meals0003.png";
 import burger from "../assets/burger.jpg";
-import meals004 from "../assets/mels0005.avif"
-import meals0005 from "../assets/meals0004.avif"
+import meals004 from "../assets/mels0005.avif";
+import meals0005 from "../assets/meals0004.avif";
+import { AnimatePresence, motion } from "motion/react";
+import { X } from "lucide-react";
 
-export default function ProductCart({image , price , description , name}) {
+export default function ProductCart({ image, price, description, name }) {
+  const [open, setOpen] = useState(false);
+  const item = {
+    exit: {
+      opacity: 0,
+      height: 0,
+      transition: {
+        ease: "easeInOut",
+        duration: 0.2,
+       
+      },
+    },
+  };
+
   return (
     <div className="h-[100px]">
-      <div className="flex gap-[18px]">
+      <div className="flex gap-[18px] container"  onClick={() => setOpen(true)}>
         <div>
           <h2 className="font-[500]">{name}</h2>
           <p className="text-[12px] overflow-hidden h-10 text-ellipsis text-clip">
@@ -17,10 +32,33 @@ export default function ProductCart({image , price , description , name}) {
           <h4 className="font-[500] text-[14px] pt-[6px]">{price} MAD</h4>
         </div>
         <div className="  ">
-          <img src={image} className=" w-[300px] h-full rounded-[10px]"/>
+          <img src={image} className=" w-[300px] h-full rounded-[10px]" />
         </div>
       </div>
-      <hr className="mt-3"/>
+      <hr className="mt-3" />
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            className=" z-[2000] bg-white  fixed w-full h-[100vh] overflow-hidden top-0"
+            // variants={item}
+            // initial={{ height: 0, opacity: 0 }}
+            // animate={{ height: "100vh", opacity: 1 }}
+            // transition={{ duration: 0.5 }}
+            // exit="exit"
+          >
+            
+            <div className="relative">
+              <img src={image} className="rounded-b-[50px] w-full" />
+              <X className="absolute top-2 h-8 w-8 right-2 bg-white p-1 rounded-full cursor-pointer" onClick={() => setOpen(false)}/>
+            </div>
+            <div className="container flex flex-col gap-[5px] pt-3">  
+              <h1 className="font-[600] text-[20px]">{name}</h1>
+              <span className="font-[600] ">{price} $</span>
+              <p className="text-[14px]">{description}</p>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
